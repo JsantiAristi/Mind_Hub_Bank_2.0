6777b1fce2fbcf3a9c0887ff7c23c2b277e5e056
Santi <jsanti.restrepo.2021@gmail.com>
2023-04-18 23:12:31 -0500

    Corrijo el carrousel de usuarios.

--- src/main/resources/static/web/js/index.js
+++ src/main/resources/static/web/js/index.js
@@ -9,14 +9,38 @@ createApp({
     },
 }).mount("#app");
 
-const swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-        type: 'bullets',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+const mq = window.matchMedia("(max-width: 700px)")
+
+function pantallaPequena(mq){
+    if (mq.matches) {
+        const swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    } else {
+        const swiper = new Swiper(".mySwiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+}
+
+mq.addListener(pantallaPequena);
+
+pantallaPequena(mq);
